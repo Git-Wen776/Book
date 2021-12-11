f50d4f12fc735093f3322577dbf8ad1b0c14cfe3
Aw <316452220@qq.com>
2021-12-11 17:25:34 +0800

    添加正则表达式模型验证类

---
+++ Book.Models/ModelValidation/RegexValidaProperty.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Book.Models.ModelValidation
+{
+    public class RegexValidaProperty<T> : PropertyValidator<T, string>
+    {
+        public override string Name => "RegesValidaProperty";
+
+        private string regestr;
+
+        public RegexValidaProperty(string _regesStr)
+        {
+            regestr= _regesStr;
+        }
+
+        public override bool IsValid(ValidationContext<T> context, string value)
+        {
+            if(value == null)
+                throw new ArgumentNullException(nameof(value));
+            if (value.Equals(""))
+                throw new ArgumentException("value is not empty string");
+            Regex regex=new Regex(regestr);
+            return regex.IsMatch(value);
+
+        }
+    }
+}
---
+++ Book.Models/ModelValidation/RegexValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Book.Models.ModelValidation
+{
+    public static class RegexValidation
+    {
+        public static IRuleBuilderOptionsConditions<T,string> RegexValida<T>(this IRuleBuilderOptions<T,string> options,string regesstring)
+        {
+            return options.Custom((obj, context) =>
+            {
+                if (regesstring == null)
+                    throw new ArgumentNullException(nameof(regesstring));
+                if (obj.GetType() != typeof(string))
+                    throw new ArgumentException("regesvalida regex must be string");
+                Regex reges = new Regex(regesstring);
+                if (!reges.IsMatch(obj.ToString()))
+                    context.AddFailure("");
+            });
+        }
+    }
+
+}
